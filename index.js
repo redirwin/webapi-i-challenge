c805d1ede9219548695c4bc288dd88153bc75bc7
Dave Irwin <david@redirwin.com>
2019-08-05 17:52:50 -0700

    All CRUD actions operating successfully.

--- index.js
+++ index.js
@@ -36,10 +36,73 @@ server.get("/api/users", (req, res) => {
       console.log(result);
     })
     .catch(error => {
-      res
-        .status(500)
-        .json({ error: "The users information could not be retrieved." });
+      res.status(500).json({ error: "The users list could not be retrieved." });
+      console.log(error);
     });
+});
+
+server.get("/api/users/:id", (req, res) => {
+  const id = req.params.id;
+  Users.findById(id)
+    .then(result => {
+      if (result) {
+        res.status(200).json(result);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+      }
+    })
+    .catch(error => {
+      res
+        .status(505)
+        .json({ error: "The user information could not be retrieved." });
+    });
+});
+
+server.delete("/api/users/:id", (req, res) => {
+  const id = req.params.id;
+  Users.remove(id)
+    .then(result => {
+      if (result) {
+        res.status(200).json({ message: "Deleted!" });
+      } else {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error: "The user could not be removed." });
+    });
+});
+
+server.put("/api/users/:id", (req, res) => {
+  const body = req.body;
+  const id = req.params.id;
+
+  if (!body.name || !body.bio) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  } else {
+    Users.update(id, body)
+      .then(result => {
+        if (result) {
+          res.status(200).json({ message: "Updated!" });
+        } else {
+          res.status(404).json({
+            message: "The user with the specified ID does not exist."
+          });
+        }
+      })
+      .catch(error => {
+        console.log(error);
+        res
+          .status(500)
+          .json({ error: "The user information could not be modified." });
+      });
+  }
 });
 
 const port = 5000;
